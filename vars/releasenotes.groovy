e3b9fafe7383f0b2d802c2a86d12a46d1fcc1ab1
Ahmed Gharib <a.gharib89@yahoo.com>
2021-10-21 19:22:50 +0200

    Update groovy file

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,32 +1,47 @@
+
 import groovy.io.FileType;
 import java.io.File;
+import java.util.Calendar.*;
 import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
-def call(Map config=[:]){
+def call(Map config=[:])
+{
     def dir = new File(pwd());
-    dir.eachFileRecurse(FileType.ANY){ file ->
-        println(file.name)
-    }
+
     new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
     { 
         writer -> 
+        
+        def now = new Date();
+        def fmt = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        writer.writeLine("Date and Time IS: " + fmt.format(now));
+
+        writer.writeLine("Build Number is: ${BUILD_NUMBER}");
+
+        if (config.changes != "false"){
+            def changeLogSets = currentBuild.changeSets;
+
+            for (change in changeLogSets) {
+                def entries = change.items;
+                for (entry in entries) {
+                    writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}");
+                    for (file in entry.affectedFiles) {
+                        writer.writeLine("${file.editType.name} ${file.path}");
+                    }
+                }
+            }
+        }
+        
         dir.eachFileRecurse(FileType.ANY){ file ->
-        if (file.isDirectory()){
-            writer.writeLine(file.name);            
-        }
-        else
-        {
-            writer.writeLine('\t' + file.name + '\t' + file.length());
-        }
+            if (file.isDirectory()){
+                writer.writeLine(file.name);            
+            }
+            else
+            {
+                writer.writeLine('\t' + file.name + '\t' + file.length());
+            }        
         } 
-    }
-    
-    def date = new Date()
-    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-    echo "Date and Time IS: " + sdf.format(date)
-    
-    if (config.changes != "false"){
-        echo "changes";
     }
 }
